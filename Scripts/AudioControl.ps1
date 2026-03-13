0a268ab8f8b298df57e4bab0c17cdb6fc679f859
Stuart Bonnell <stuart.bonnell@sarasin.co.uk>
2026-03-13 18:12:55 +0000

    Update GUID for IMMDeviceEnumerator interface in AudioControl.ps1

--- Scripts/AudioControl.ps1
+++ Scripts/AudioControl.ps1
@@ -94,7 +94,7 @@ namespace AudioControl
         All        = 0xF
     }
 
-    [Guid("BCDE0395-E52F-467C-8E3D-C4579291692E")]
+    [Guid("A95664D2-9614-4F35-A746-DE8DB63617E6")]
     [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
     public interface IMMDeviceEnumerator
     {
